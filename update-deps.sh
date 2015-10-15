012362f58b9733fde2bbcfd38b43499ab4b47674
Dallan Quass <dallan@quass.org>
2015-10-15 16:06:36 -0600

    add github.com/jtolds/gls dependency

---
+++ update-deps.sh
@@ -0,0 +1,2 @@
+git subtree pull --prefix _vendor/src/github.com/jtolds/gls https://github.com/jtolds/gls master --squash
+git subtree pull --prefix _vendor/src/github.com/smartystreets/goconvey https://github.com/smartystreets/goconvey master --squash
