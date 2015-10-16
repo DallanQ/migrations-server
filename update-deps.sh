39b7e5c773c1ded4bdfb0b80e2e168d024cec574
Dallan Quass <dallan@quass.org>
2015-10-15 20:55:30 -0600

    update dependencies for echo

--- update-deps.sh
+++ update-deps.sh
@@ -1,6 +1,12 @@
 git subtree pull --prefix _vendor/src/github.com/jtolds/gls https://github.com/jtolds/gls master --squash
 git subtree pull --prefix _vendor/src/github.com/smartystreets/assertions https://github.com/smartystreets/assertions master --squash
 git subtree pull --prefix _vendor/src/github.com/smartystreets/goconvey https://github.com/smartystreets/goconvey master --squash
+
 git subtree pull --prefix _vendor/src/github.com/go-sql-driver/mysql https://github.com/go-sql-driver/mysql master --squash
+
 git subtree pull --prefix _vendor/src/github.com/jmoiron/sqlx https://github.com/jmoiron/sqlx master --squash
+
+git subtree pull --prefix _vendor/src/github.com/bradfitz/http2 https://github.com/bradfitz/http2 master --squash
+git subtree pull --prefix _vendor/src/github.com/labstack/gommon https://github.com/labstack/gommon master --squash
+git subtree pull --prefix _vendor/src/golang.org/x/net https://github.com/golang/net master --squash
 git subtree pull --prefix _vendor/src/github.com/labstack/echo https://github.com/labstack/echo master --squash
