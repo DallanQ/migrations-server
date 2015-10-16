7dc2045b0cec2bfbc4086f54b898ae4065fea3bc
Dallan Quass <dallan@quass.org>
2015-10-16 11:46:27 -0600

    working server reads from db

--- main.go
+++ main.go
@@ -1,23 +1,104 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"github.com/kelseyhightower/envconfig"
 	"github.com/labstack/echo"
 	mw "github.com/labstack/echo/middleware"
 )
 
-func hello(c *echo.Context) error {
-	return c.String(http.StatusOK, "Hello World\n")
+type Params struct {
+	Port string `envconfig:"PORT" required:"true"`
+	DSN  string `required:"true"`
 }
 
 func main() {
+	// parse params
+	var params Params
+	if err := envconfig.Process("fsmigrations", &params); err != nil {
+		log.Fatal("Error parsing parameters", err)
+	}
+	log.Printf("Params %#v\n", params)
+
+	// init db
+	db, err := sqlx.Open("mysql", params.DSN)
+	if err != nil {
+		log.Fatal("Error opening db", err)
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		log.Println("Error pinging db", err)
+	}
+
+	// init server
+	log.Println("Start server")
 	e := echo.New()
 
 	e.Use(mw.Logger())
 	e.Use(mw.Recover())
 
-	e.Get("/", hello)
+	e.Get("/", func(c *echo.Context) error {
+		return c.String(http.StatusOK, "Ok")
+	})
 
-	e.Run(":8000")
+	e.Get("/immigrations", func(c *echo.Context) error {
+		place := c.Query("place")
+		year := c.Query("year")
+
+		placeCounts, err := getImmigrations(db, place, year)
+		if err != nil {
+			return err
+		}
+		return c.JSON(http.StatusOK, placeCounts)
+	})
+
+	e.Get("/emigrations", func(c *echo.Context) error {
+		place := c.Query("place")
+		year := c.Query("year")
+
+		placeCounts, err := getEmigrations(db, place, year)
+		if err != nil {
+			return err
+		}
+		return c.JSON(http.StatusOK, placeCounts)
+	})
+
+	e.Run(":" + params.Port)
+}
+
+type DbSelecter interface {
+	Select(dest interface{}, query string, args ...interface{}) error
+}
+
+type ImmigrationPlaceCount struct {
+	Place string `json:"place" db:"place_from"`
+	Count int    `json:"count" db:"count"`
+}
+
+func getImmigrations(db DbSelecter, place, year string) ([]ImmigrationPlaceCount, error) {
+	placeCounts := []ImmigrationPlaceCount{}
+	err := db.Select(&placeCounts, "SELECT place_from, count FROM immigrations WHERE place_to = ? and year = ?", place, year)
+	if err != nil {
+		return nil, err
+	}
+	return placeCounts, nil
+}
+
+type EmigrationPlaceCount struct {
+	Place string `json:"place" db:"place_to"`
+	Count int    `json:"count" db:"count"`
+}
+
+func getEmigrations(db DbSelecter, place, year string) ([]EmigrationPlaceCount, error) {
+	placeCounts := []EmigrationPlaceCount{}
+	err := db.Select(&placeCounts, "SELECT place_to, count FROM emigrations WHERE place_from = ? and year = ?", place, year)
+	if err != nil {
+		return nil, err
+	}
+	return placeCounts, nil
 }
--- update-deps.sh
+++ update-deps.sh
@@ -6,6 +6,8 @@ git subtree pull --prefix _vendor/src/github.com/go-sql-driver/mysql https://git
 
 git subtree pull --prefix _vendor/src/github.com/jmoiron/sqlx https://github.com/jmoiron/sqlx master --squash
 
+git subtree pull --prefix _vendor/src/github.com/kelseyhightower/envconfig https://github.com/kelseyhightower/envconfig master --squash
+
 git subtree pull --prefix _vendor/src/github.com/bradfitz/http2 https://github.com/bradfitz/http2 master --squash
 git subtree pull --prefix _vendor/src/github.com/labstack/gommon https://github.com/labstack/gommon master --squash
 git subtree pull --prefix _vendor/src/golang.org/x/net https://github.com/golang/net master --squash
